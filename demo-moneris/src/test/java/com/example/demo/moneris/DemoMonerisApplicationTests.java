81db69ca2d6e3067e59ecac4303d4c72dfbba295
Shawn Yang <shawn.yang@idtecproducts.com>
2021-07-30 13:59:03 +0800

    first commit

---
+++ demo-moneris/src/test/java/com/example/demo/moneris/DemoMonerisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.moneris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMonerisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
